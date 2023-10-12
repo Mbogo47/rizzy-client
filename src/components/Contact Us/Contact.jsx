c40d57f96adf70cebc71f433d587b2917106c9fb
Ivy Mbogo <97885622+Mbogo47@users.noreply.github.com>
2023-10-12 18:21:29 +0300

    Update Contact.jsx

--- src/components/Contact Us/Contact.jsx
+++ src/components/Contact Us/Contact.jsx
@@ -32,20 +32,7 @@ const Contact = () => {
           </div>
 
           <div className="row">
-             
-
-              <div className="image">
-                    <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.623478986334!2d37.31768627412843!3d-0.5662437352612878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182629b5a37f6381%3A0x859cebe37dc37639!2sKirinyaga%20University!5e0!3m2!1sen!2ske!4v1697118456397!5m2!1sen!2ske"
-          width="600"
-          height="450"
-          style={{border: "0"}}
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe>
-                </div>
-             <form action="">
+ <form action="">
                   <h3>get in touch</h3>
                   <div className="inputBox">
                       <input type="text" placeholder="your name" />
@@ -58,7 +45,10 @@ const Contact = () => {
                   <textarea placeholder="your message" cols="30" rows="10"></textarea>
                   <input type="submit" value="send message" className="btn" />
               </form>
-
+            
+              <div className="image">
+                    <img src ={contactpic} alt="" />
+                </div>
           </div>
       </section>
   )
